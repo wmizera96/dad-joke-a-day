43770a2148bc5a10e05071a27021f546ea92241e
wmizera96 <wmizera@outlook.com>
2022-11-20 20:22:02 +0100

    Initial commit

---
+++ Functions/Core/IJokeService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Functions.Core
+{
+    internal interface IJokeService
+    {
+        Task<Joke> GetJokeAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ Functions/Core/JokesService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Functions.Core
+{
+    internal class JokeService : IJokeService
+    {
+        public Task<Joke> GetJokeAsync(CancellationToken cancellationToken)
+        {
+            // TODO implement
+            return Task.FromResult<Joke>(new ());
+        }
+    }
+}
