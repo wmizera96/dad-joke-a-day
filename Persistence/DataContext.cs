39c37b3a51bc0169a89557307ab2f17022fb504c
wmizera96 <wmizera@outlook.com>
2022-11-28 23:01:38 +0100

    add domain and persistence projects

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+
+
+public class DataContext : IdentityDbContext<AppUser>
+{
+    public DbSet<Joke>? Jokes { get; set; }   
+
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+}
